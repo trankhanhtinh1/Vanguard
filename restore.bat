400e5d0b75bfc026372497b7bd385d9d50663caf
unknown <MR THINH@SEGQ-QW1CWQ.1513822835-160023780-85425026>
2025-08-20 09:13:55 +0700

    updated

---
+++ restore.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Patching LeagueClient.exe...
+
+REM Patch /lol-vanguard/v1/config/..abled -> /lol-vanguard/v1/config/enabled
+powershell -Command "(Get-Content 'LeagueClient.exe' -Raw) -replace '/lol-vanguard/v1/config/..abled', '/lol-vanguard/v1/config/enabled' | Set-Content 'LeagueClient.exe'"
+
+echo Patch completed!
+pause
